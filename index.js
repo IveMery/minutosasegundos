90fc7af17fc9f00586a381c2315d29241ba8dd05
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:18:58 -0400

    minutosseg

---
+++ index.js
@@ -0,0 +1,8 @@
+// Minutos a segundos
+alert ("Debes ingresar  una cantidad de minutos que quieras y la pasaremos a segundos")
+let minutosSegundos = prompt("Ingresa los minutos que desees convertir")
+
+
+let segundos = (minutosSegundos * 60 )
+
+alert  (`  ${minutosSegundos} Minutos son ${segundos} Segundos `)
